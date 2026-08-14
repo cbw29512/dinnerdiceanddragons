b2c55a851be891e3699097815c2f9dfaba16e712
Chris Wilson <divclass01@gmail.com>
2026-08-14 16:28:53 -0400

    Add browser test helpers

---
+++ e2e/helpers.js
@@ -0,0 +1,49 @@
+const { expect } = require("@playwright/test");
+
+const ZIP_COORDINATES = Object.freeze({
+  "29501": { city: "Florence", state: "SC", latitude: "34.1954", longitude: "-79.7626" },
+  "29505": { city: "Florence", state: "SC", latitude: "34.1450", longitude: "-79.7700" },
+  "29506": { city: "Florence", state: "SC", latitude: "34.2250", longitude: "-79.7350" }
+});
+
+async function mockZipLookup(page) {
+  try {
+    await page.route("https://api.zippopotam.us/us/**", async (route) => {
+      const zip = new URL(route.request().url()).pathname.split("/").filter(Boolean).pop();
+      const location = ZIP_COORDINATES[zip] || ZIP_COORDINATES["29501"];
+      await route.fulfill({
+        status: 200,
+        contentType: "application/json",
+        body: JSON.stringify({
+          "post code": zip,
+          country: "United States",
+          "country abbreviation": "US",
+          places: [{
+            "place name": location.city,
+            "state abbreviation": location.state,
+            latitude: location.latitude,
+            longitude: location.longitude
+          }]
+        })
+      });
+    });
+  } catch (error) {
+    console.error("Unable to install ZIP lookup mock", error);
+    throw error;
+  }
+}
+
+async function expectNoHorizontalOverflow(page) {
+  try {
+    const dimensions = await page.evaluate(() => ({
+      scrollWidth: document.documentElement.scrollWidth,
+      clientWidth: document.documentElement.clientWidth
+    }));
+    expect(dimensions.scrollWidth).toBeLessThanOrEqual(dimensions.clientWidth + 1);
+  } catch (error) {
+    console.error("Unable to check horizontal overflow", error);
+    throw error;
+  }
+}
+
+module.exports = { mockZipLookup, expectNoHorizontalOverflow };
