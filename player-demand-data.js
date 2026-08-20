89828ce910a6457804dcbb6f511c43fd2f8ec04f
Chris Wilson <divclass01@gmail.com>
2026-08-19 23:24:32 -0400

    Remove hard-coded sample player demand

--- player-demand-data.js
+++ player-demand-data.js
@@ -1,13 +1,2 @@
-window.DDD_PLAYER_DEMAND = [
-  { id: "p1", system: "D&D 5e", day: "Tuesday", start: "18:00", end: "22:00", zip: "29501", radius: 25 },
-  { id: "p2", system: "D&D 5e", day: "Tuesday", start: "18:00", end: "22:00", zip: "29501", radius: 10 },
-  { id: "p3", system: "D&D 5e", day: "Tuesday", start: "18:00", end: "22:00", zip: "29505", radius: 25 },
-  { id: "p4", system: "D&D 5e", day: "Tuesday", start: "18:00", end: "22:00", zip: "29506", radius: 25 },
-  { id: "p5", system: "D&D 5e", day: "Tuesday", start: "18:00", end: "22:00", zip: "29501", radius: 50 },
-  { id: "p6", system: "Pathfinder 2e", day: "Saturday", start: "13:00", end: "18:00", zip: "29501", radius: 25 },
-  { id: "p7", system: "Pathfinder 2e", day: "Saturday", start: "13:00", end: "18:00", zip: "29505", radius: 25 },
-  { id: "p8", system: "Pathfinder 2e", day: "Saturday", start: "13:00", end: "18:00", zip: "29506", radius: 25 },
-  { id: "p9", system: "Call of Cthulhu", day: "Friday", start: "18:00", end: "22:00", zip: "29501", radius: 25 },
-  { id: "p10", system: "Call of Cthulhu", day: "Friday", start: "18:00", end: "22:00", zip: "29505", radius: 25 },
-  { id: "p11", system: "Call of Cthulhu", day: "Friday", start: "18:00", end: "22:00", zip: "29505", radius: 10 }
-];
+// Production demand starts empty. Real Player demand is persisted through Netlify Database.
+window.DDD_PLAYER_DEMAND = [];
